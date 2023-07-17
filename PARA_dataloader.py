b4c69bdb50ef77343329724c499330069fe8486a
lwchen <li-wei.chen@kuleuven.be>
2023-07-17 11:01:33 +0200

    Add score histogram to PARA datasets

--- PARA_dataloader.py
+++ PARA_dataloader.py
@@ -9,10 +9,12 @@ import numpy as np
 
 
 class PARADataset(Dataset):
-    def __init__(self, root_dir, transform=None, train=True, random_seed=None, use_attr=True):
+    def __init__(self, root_dir, transform=None, train=True, random_seed=None, 
+                 use_attr=True, use_hist=True):
         self.root_dir = root_dir
         self.transform = transform
         self.use_attr = use_attr
+        self.use_hist = use_hist
         self.train = train
         if self.train:
             self.annotations = pd.read_csv(os.path.join(root_dir, 'annotation', 'PARA-GiaaTrain.csv'))
@@ -21,7 +23,7 @@ class PARADataset(Dataset):
             if random_seed is not None:
                 random.seed(random_seed)
                 torch.manual_seed(random_seed)
-
+        
         self.attributes = [
             'aestheticScore',
             'qualityScore',
@@ -33,10 +35,20 @@ class PARADataset(Dataset):
             'contentPreference',
             'willingnessToShare'
         ]
+        if self.use_hist:
+            self.attributes = [self.attributes[0],*self.attributes[2:]]
         if not self.use_attr:
             self.attributes = [self.attributes[0]]
+
         self.mean_attributes = ['%s_mean'%x for x in self.attributes]
         self.std_attributes = ['%s_std'%x for x in self.attributes]
+        scale = np.arange(1, 5.5, 0.5)
+        self.score_hist = []
+        self.score_hist.append(['%s_%2.1f'%(self.attributes[0], i) for i in scale])
+        if self.use_attr:
+            for attr in self.attributes[1:]:
+                scale = np.arange(1, 5.5, 1.0)
+                self.score_hist.append(['%s_%d'%(attr, i) for i in scale])
 
     def __len__(self):
         return len(self.annotations)
@@ -45,8 +57,10 @@ class PARADataset(Dataset):
         annotations = self.annotations
         session_dir = annotations.iloc[idx]['sessionId']
         img_path = os.path.join(self.root_dir, 'imgs', session_dir, annotations.iloc[idx]['imageName'])
-        # aesthetic_score_mean = annotations.iloc[idx]['aestheticScore_mean']
-        # aesthetic_score_std = annotations.iloc[idx]['aestheticScore_std']
+        if self.use_hist:
+            aesthetic_score_hist = [np.array(annotations.iloc[idx][attr], dtype=np.float32) for attr in self.score_hist]
+        else:
+            aesthetic_score_hist = None
         aesthetic_score_mean = np.array(annotations.iloc[idx][self.mean_attributes], dtype=np.float32)
         aesthetic_score_std = np.array(annotations.iloc[idx][self.std_attributes], dtype=np.float32)
         
@@ -55,7 +69,7 @@ class PARADataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        return image, aesthetic_score_mean, aesthetic_score_std
+        return image, aesthetic_score_mean, aesthetic_score_std, aesthetic_score_hist
 
 
 if __name__ == '__main__':
@@ -76,18 +90,19 @@ if __name__ == '__main__':
     ])
 
     # Create datasets with the appropriate transformations
-    train_dataset = PARADataset(root_dir, transform=train_transform, train=True, use_attr=False)
-    test_dataset = PARADataset(root_dir, transform=test_transform, train=False, use_attr=False, random_seed=random_seed)
+    train_dataset = PARADataset(root_dir, transform=train_transform, train=True, use_attr=True)
+    test_dataset = PARADataset(root_dir, transform=test_transform, train=False, use_attr=True, random_seed=random_seed)
 
     # Create dataloaders for training and test sets
     train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
     # Iterate over the training dataloader
-    for images, mean_scores, std_scores in train_dataloader:
+    for images, mean_scores, std_scores, score_hist in train_dataloader:
         # Perform training operations here
         print(mean_scores)
         print(std_scores)
+        print(score_hist)
         raise Exception
 
     # Iterate over the test dataloader
---
+++ train_resnet_giaa.py
@@ -0,0 +1,176 @@
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.models import resnet50
+import numpy as np
+from tqdm import tqdm
+from PARA_dataloader import PARADataset
+import wandb
+
+
+def train(model, train_dataloader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+
+    progress_bar = tqdm(train_dataloader, leave=False)
+    for images, mean_scores, std_scores in progress_bar:
+        images = images.to(device)
+        mean_scores = mean_scores.to(device)
+        optimizer.zero_grad()
+
+        outputs = model(images)
+        loss = criterion(outputs, mean_scores)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        progress_bar.set_postfix({'Train Loss': loss.item()})
+        
+    epoch_loss = running_loss / len(train_dataloader)
+    return epoch_loss
+
+
+def evaluate(model, dataloader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+
+    progress_bar = tqdm(dataloader, leave=False)
+    with torch.no_grad():
+        for images, mean_scores, std_scores in progress_bar:
+            images = images.to(device)
+            mean_scores = mean_scores.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs[:,0][:,None], mean_scores[:,0][:,None])
+            # loss = criterion(outputs, mean_scores)
+
+            running_loss += loss.item()
+            progress_bar.set_postfix({'Eval Loss': loss.item()})
+
+    epoch_loss = running_loss / len(dataloader)
+    return epoch_loss
+
+
+if __name__ == '__main__':
+    # Set random seed for reproducibility
+    random_seed = 42
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+
+    is_log = False
+    use_attr = True
+    lr = 1e-3
+    batch_size = 32
+    num_epochs = 30
+    if is_log:
+        wandb.init(project="resnet_PARA")
+        wandb.config = {
+        "learning_rate": lr,
+        "batch_size": batch_size,
+        "num_epochs": num_epochs
+        }
+
+    # Define the root directory of the PARA dataset
+    root_dir = '/home/lwchen/datasets/PARA/'
+
+    # Define transformations for training set and test set
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.ToTensor(),
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
+
+    # Create datasets with the appropriate transformations
+    train_dataset = PARADataset(root_dir, transform=train_transform, train=True, use_attr=use_attr, random_seed=random_seed)
+    test_dataset = PARADataset(root_dir, transform=test_transform, train=False, use_attr=use_attr, random_seed=random_seed)
+
+    # Create dataloaders for training and test sets
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    # Define the number of classes in your dataset
+    if use_attr:
+        num_classes = 9
+    else:
+        num_classes = 1
+
+    # Define the device for training
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load the pre-trained ResNet model
+    model_resnet50 = resnet50(pretrained=True)
+
+    # Modify the last fully connected layer to match the number of classes
+    num_features = model_resnet50.fc.in_features
+    model_resnet50.fc = nn.Linear(num_features, num_classes)
+    model_resnet50.load_state_dict(torch.load(
+        os.path.join('./1e-3_30epoch', 'best_model_resnet50.pth')))
+
+    # Move the model to the device
+    model_resnet50 = model_resnet50.to(device)
+
+    # Define the loss function
+    criterion = nn.MSELoss()
+
+    # Define the optimizer
+    optimizer_resnet50 = optim.SGD(model_resnet50.parameters(), lr=lr, momentum=0.9)
+
+    # Define a list to record the training and test losses
+    train_loss_list = []
+    test_loss_list = []
+
+    # Initialize the best test loss and the best model
+    best_test_loss = float('inf')
+    best_model = None
+        
+    # Training loop for ResNet-50
+    for epoch in range(num_epochs):
+        # Training
+        # train_loss = train(model_resnet50, train_dataloader, criterion, optimizer_resnet50, device)
+        # train_loss_list.append(train_loss)
+        # if is_log:
+        #     wandb.log({"Train Loss": train_loss})
+
+        # Testing
+        test_loss = evaluate(model_resnet50, test_dataloader, criterion, device)
+        test_loss_list.append(test_loss)
+        if is_log:
+            wandb.log({"Test Loss": test_loss}, commit=False)
+        print(test_loss)
+        raise Exception
+
+        # Print the epoch loss
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss}, Test Loss: {test_loss}")
+
+        # Check if the current model has the best test loss so far
+        if test_loss < best_test_loss:
+            best_test_loss = test_loss
+            best_model = model_resnet50.state_dict()
+    
+    # Save the best model
+    best_modelname = 'best_model_resnet50'
+    if not use_attr:
+        best_modelname += '_noattr'
+    best_modelname += '.pth'
+    torch.save(best_model, best_modelname)
+
+    # Record the training and test losses into a text file
+    loss_records = {'Train Loss': train_loss_list, 'Test Loss': test_loss_list}
+    pd.DataFrame(loss_records).to_csv('loss_records.txt', index=False)
+
+    # At this point, ResNet-50 has been fine-tuned on the PARA dataset, the best model has been saved,
+    # and the training and test losses have been recorded into a text file.
