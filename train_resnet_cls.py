b9c87c77f5973cb60ebc54b64a71ba6e4a8eb7b9
lwchen <li-wei.chen@kuleuven.be>
2023-07-20 11:39:53 +0200

    Add raw CE as metric

--- train_resnet_cls.py
+++ train_resnet_cls.py
@@ -25,9 +25,10 @@ def earth_mover_distance(x, y):
     return torch.mean(emd)
 
 
-def train(model, train_dataloader, criterion_ce, criterion_mse, criterion_emd, optimizer, device):
+def train(model, train_dataloader, criterion_weight_ce, criterion_raw_ce, criterion_mse, criterion_emd, optimizer, device):
     model.train()
     running_ce_loss = 0.0
+    running_raw_ce_loss = 0.0
     running_mse_mean_loss = 0.0
     running_mse_std_loss = 0.0
     running_emd_loss = 0.0
@@ -45,7 +46,8 @@ def train(model, train_dataloader, criterion_ce, criterion_mse, criterion_emd, o
         outputs = F.softmax(outputs, dim=1)
 
         # Cross-entropy loss
-        ce_loss = criterion_ce(outputs, score_prob)
+        ce_loss = criterion_weight_ce(outputs, score_prob)
+        raw_ce_loss = criterion_raw_ce(outputs, score_prob)
 
         # Earth Mover's Distance (EMD) loss
         emd_loss = criterion_emd(outputs, score_prob)
@@ -62,32 +64,37 @@ def train(model, train_dataloader, criterion_ce, criterion_mse, criterion_emd, o
             loss = ce_loss
         else:
             loss = emd_loss
+
         loss.backward()
         optimizer.step()
 
         running_ce_loss += ce_loss.item()
+        running_raw_ce_loss += raw_ce_loss.item()
         running_mse_mean_loss += mse_mean_loss.item()
         running_mse_std_loss += mse_std_loss.item()
         running_emd_loss += emd_loss.item()
 
         progress_bar.set_postfix({
             'Train CE Loss': ce_loss.item(),
+            'Train Raw CE Loss': raw_ce_loss.item(),
             # 'Train MSE Mean Loss': mse_mean_loss.item(),
             # 'Train MSE Std Loss': mse_std_loss.item(),
             'Train EMD Loss': emd_loss.item()
         })
 
     epoch_ce_loss = running_ce_loss / len(train_dataloader)
+    epoch_raw_ce_loss = running_raw_ce_loss / len(train_dataloader)
     epoch_mse_mean_loss = running_mse_mean_loss / len(train_dataloader)
     epoch_mse_std_loss = running_mse_std_loss / len(train_dataloader)
     epoch_emd_loss = running_emd_loss / len(train_dataloader)
 
-    return epoch_ce_loss, epoch_mse_mean_loss, epoch_mse_std_loss, epoch_emd_loss
+    return epoch_ce_loss, epoch_raw_ce_loss, epoch_mse_mean_loss, epoch_mse_std_loss, epoch_emd_loss
 
 
-def evaluate(model, dataloader, criterion_ce, criterion_mse, criterion_emd, device):
+def evaluate(model, dataloader, criterion_weight_ce, criterion_raw_ce, criterion_mse, criterion_emd, device):
     model.eval()
     running_ce_loss = 0.0
+    running_raw_ce_loss = 0.0
     running_mse_mean_loss = 0.0
     running_mse_std_loss = 0.0
     running_emd_loss = 0.0
@@ -105,7 +112,8 @@ def evaluate(model, dataloader, criterion_ce, criterion_mse, criterion_emd, devi
             outputs = F.softmax(outputs, dim=1)
 
             # Cross-entropy loss
-            ce_loss = criterion_ce(outputs, score_prob)
+            ce_loss = criterion_weight_ce(outputs, score_prob)
+            raw_ce_loss = criterion_raw_ce(outputs, score_prob)
 
             # MSE loss for mean
             outputs_mean = torch.sum(outputs * scale, dim=1, keepdim=True)
@@ -119,23 +127,26 @@ def evaluate(model, dataloader, criterion_ce, criterion_mse, criterion_emd, devi
             emd_loss = criterion_emd(outputs, score_prob)
 
             running_ce_loss += ce_loss.item()
+            running_raw_ce_loss += raw_ce_loss.item()
             running_mse_mean_loss += mse_mean_loss.item()
             running_mse_std_loss += mse_std_loss.item()
             running_emd_loss += emd_loss.item()
 
             progress_bar.set_postfix({
                 'Eval CE Loss': ce_loss.item(),
+                'Eval Raw CE Loss': raw_ce_loss.item(),
                 # 'Eval MSE Mean Loss': mse_mean_loss.item(),
                 # 'Eval MSE Std Loss': mse_std_loss.item(),
                 'Eval EMD Loss': emd_loss.item()
             })
 
     epoch_ce_loss = running_ce_loss / len(dataloader)
+    epoch_raw_ce_loss = running_raw_ce_loss / len(dataloader)
     epoch_mse_mean_loss = running_mse_mean_loss / len(dataloader)
     epoch_mse_std_loss = running_mse_std_loss / len(dataloader)
     epoch_emd_loss = running_emd_loss / len(dataloader)
 
-    return epoch_ce_loss, epoch_mse_mean_loss, epoch_mse_std_loss, epoch_emd_loss
+    return epoch_ce_loss, epoch_raw_ce_loss, epoch_mse_mean_loss, epoch_mse_std_loss, epoch_emd_loss
 
 
 is_log = True
@@ -202,14 +213,15 @@ if __name__ == '__main__':
     # Modify the last fully connected layer to match the number of classes
     num_features = model_resnet50.fc.in_features
     model_resnet50.fc = nn.Linear(num_features, num_classes)
-    
+
     # Move the model to the device
     model_resnet50 = model_resnet50.to(device)
 
     # Define the loss functions
     ce_weight = 1 / train_dataset.aesthetic_score_hist_prob
-    ce_weight = ce_weight / np.sum(ce_weight)
-    criterion_ce = nn.CrossEntropyLoss(weight=torch.tensor(ce_weight, device=device))
+    ce_weight = ce_weight / np.sum(ce_weight) * len(ce_weight)
+    criterion_weight_ce = nn.CrossEntropyLoss(weight=torch.tensor(ce_weight, device=device))
+    criterion_raw_ce = nn.CrossEntropyLoss()
     criterion_mse = nn.MSELoss()
     criterion_emd = earth_mover_distance
 
@@ -218,10 +230,12 @@ if __name__ == '__main__':
 
     # Define lists to record the training and test losses
     train_ce_loss_list = []
+    train_raw_ce_loss_list = []
     train_mse_mean_loss_list = []
     train_mse_std_loss_list = []
     train_emd_loss_list = []
     test_ce_loss_list = []
+    test_raw_ce_loss_list = []
     test_mse_mean_loss_list = []
     test_mse_std_loss_list = []
     test_emd_loss_list = []
@@ -229,7 +243,7 @@ if __name__ == '__main__':
     # Initialize the best test loss and the best model
     best_test_loss = float('inf')
     best_model = None
-    best_modelname = 'best_model_resnet50_cls_lr%1.0e_%depoch'%(lr, num_epochs)
+    best_modelname = 'best_model_resnet50_cls_lr%1.0e_%depoch' % (lr, num_epochs)
     if not use_attr:
         best_modelname += '_noattr'
     if use_ce:
@@ -239,41 +253,46 @@ if __name__ == '__main__':
     # Training loop
     for epoch in range(num_epochs):
         # Training
-        train_ce_loss, train_mse_mean_loss, train_mse_std_loss, train_emd_loss = train(model_resnet50, train_dataloader,
-                                                                                       criterion_ce, criterion_mse,
-                                                                                       criterion_emd,
-                                                                                       optimizer_resnet50, device)
+        train_ce_loss, train_raw_ce_loss, train_mse_mean_loss, train_mse_std_loss, train_emd_loss = train(
+            model_resnet50, train_dataloader, criterion_weight_ce, criterion_raw_ce, criterion_mse, criterion_emd,
+            optimizer_resnet50, device)
         train_ce_loss_list.append(train_ce_loss)
+        train_raw_ce_loss_list.append(train_raw_ce_loss)
         train_mse_mean_loss_list.append(train_mse_mean_loss)
         train_mse_std_loss_list.append(train_mse_std_loss)
         train_emd_loss_list.append(train_emd_loss)
         if is_log:
             wandb.log({"Train CE Loss": train_ce_loss,
+                       "Train Raw CE Loss": train_raw_ce_loss,
                        "Train MSE Mean Loss": train_mse_mean_loss,
                        "Train MSE Std Loss": train_mse_std_loss,
                        "Train EMD Loss": train_emd_loss}, commit=False)
 
         # Testing
-        test_ce_loss, test_mse_mean_loss, test_mse_std_loss, test_emd_loss = evaluate(model_resnet50, test_dataloader,
-                                                                                     criterion_ce, criterion_mse,
-                                                                                     criterion_emd, device)
+        test_ce_loss, test_raw_ce_loss, test_mse_mean_loss, test_mse_std_loss, test_emd_loss = evaluate(
+            model_resnet50, test_dataloader, criterion_weight_ce, criterion_raw_ce, criterion_mse, criterion_emd,
+            device)
         test_ce_loss_list.append(test_ce_loss)
+        test_raw_ce_loss_list.append(test_raw_ce_loss)
         test_mse_mean_loss_list.append(test_mse_mean_loss)
         test_mse_std_loss_list.append(test_mse_std_loss)
         test_emd_loss_list.append(test_emd_loss)
         if is_log:
             wandb.log({"Test CE Loss": test_ce_loss,
+                       "Test Raw CE Loss": test_raw_ce_loss,
                        "Test MSE Mean Loss": test_mse_mean_loss,
                        "Test MSE Std Loss": test_mse_std_loss,
                        "Test EMD Loss": test_emd_loss})
 
         # Print the epoch loss
-        print(f"Epoch [{epoch+1}/{num_epochs}], "
+        print(f"Epoch [{epoch + 1}/{num_epochs}], "
               f"Train CE Loss: {train_ce_loss:.4f}, "
+              f"Train Raw CE Loss: {train_raw_ce_loss:.4f}, "
               f"Train MSE Mean Loss: {train_mse_mean_loss:.4f}, "
               f"Train MSE Std Loss: {train_mse_std_loss:.4f}, "
               f"Train EMD Loss: {train_emd_loss:.4f}, "
               f"Test CE Loss: {test_ce_loss:.4f}, "
+              f"Test Raw CE Loss: {test_raw_ce_loss:.4f}, "
               f"Test MSE Mean Loss: {test_mse_mean_loss:.4f}, "
               f"Test MSE Std Loss: {test_mse_std_loss:.4f}, "
               f"Test EMD Loss: {test_emd_loss:.4f}")
