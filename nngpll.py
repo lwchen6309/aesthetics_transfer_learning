Fix bug of CE computation; Remove normalization of cdf in EMD function; Merge all metric to gaussaion into one

nngpll.py
@@ -1,10 +1,7 @@
 import time
-from absl import app
 import jax.numpy as jnp
 import neural_tangents as nt
 from neural_tangents import stax
-# from examples import datasets
-from examples import util
 
 from torchvision import transforms
 from PARA_dataloader import PARADataset
@@ -14,15 +11,14 @@ from torchvision.models import resnet50
 import torch.nn as nn
 from torchvision.models.feature_extraction import create_feature_extractor
 
-
-from PIL import Image
 import numpy as np
-import torchvision
 import torch
 from tqdm import tqdm
 import os
 import matplotlib.pyplot as plt
 from act_nngp import make_dataset_imbalance
+from scipy.stats import norm
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 plt.rcParams.update({'font.size': 18})
@@ -164,6 +160,31 @@ def plot_nngp_uncertainty():
     axis.set_ylabel('MSE')
     # plt.legend()
     plt.show()
+
+
+def metric_to_cdf(scale, mu, sigma, py, ce_weight=None):
+    """
+    Compute Earth Mover's Distance (EMD) between two 1D tensors x and y using 2-norm.
+    """
+    half_res = ((scale[1] - scale[0]) * 0.5)
+    x1 = scale + half_res
+    x0 = scale - half_res
+    # mu = mu.cpu().numpy()
+    # sigma = sigma.cpu().numpy()
+    px = np.array([norm.cdf(x1, loc=_mu, scale=_sigma) - norm.cdf(x0, loc=_mu, scale=_sigma) for _mu, _sigma in zip(mu, sigma)])
+    cdf_px = np.cumsum(px, axis=1)
+    cdf_py = np.cumsum(py, axis=1)
+    emd = np.mean(np.linalg.norm(cdf_px - cdf_py, ord=2, axis=1))
+    brier_score = np.mean((px - py)**2)
+    
+    logprob = np.log(px + 1e-6)
+    raw_ce = - np.mean(np.sum(logprob * py, axis=1))
+    if ce_weight is not None:
+        ce = - np.mean(np.sum(logprob * py * ce_weight, axis=1))
+    else:
+        raw_ce = ce
+
+    return emd, brier_score, ce, raw_ce
 
 
 use_uncertainty = False
@@ -314,14 +335,16 @@ if __name__ == '__main__':
 
         batch_outputs_mean = 5 * np.array(fx_test_ntk)
         batch_outputs_std = 5 * np.abs(np.array(sigma_test_ntk)[:,None])
-        prob = np.exp(-0.5 * ((batch_outputs_mean - scale) / batch_outputs_std) ** 2) / batch_outputs_std / sqrt_2pi
-        prob = prob / np.sum(prob, axis=1)[:,None]
-        logit = np.log(prob + 1e-6)
+        # prob = np.exp(-0.5 * ((batch_outputs_mean - scale) / batch_outputs_std) ** 2) / batch_outputs_std / sqrt_2pi
+        # prob = prob / np.sum(prob, axis=1)[:,None]
+        # logit = np.log(prob + 1e-6)
 
-        ce_loss = -np.mean(np.ravel(logit * score_prob * ce_weight))
-        raw_ce_loss = -np.mean(np.ravel(logit * score_prob))
-        emd_loss = np.sqrt(np.mean(np.ravel((np.cumsum(prob,axis=1) - np.cumsum(score_prob,axis=1))**2)))
-        brier_score = np.mean(np.ravel((prob - score_prob)**2))
+        # ce_loss = -np.mean(np.ravel(logit * score_prob * ce_weight))
+        # raw_ce_loss = -np.mean(np.ravel(logit * score_prob))
+        # emd_loss = np.sqrt(np.mean(np.ravel((np.cumsum(prob,axis=1) - np.cumsum(score_prob,axis=1))**2)))
+        # brier_score = np.mean(np.ravel((prob - score_prob)**2))
+        emd_loss, brier_score, ce_loss, raw_ce_loss = metric_to_cdf(scale, batch_outputs_mean, batch_outputs_std, score_prob, ce_weight=ce_weight)
+
 
         mse_mean_loss = loss(5.0*fx_test_ntk, 5.0*y_test)
         mse_std_loss = loss(5.0*sigma_test_ntk, 5.0*y_test_std)

nngpll_cls.py
@@ -255,12 +255,12 @@ if __name__ == '__main__':
         # MSE loss for std
         outputs_std = np.sqrt(np.sum(prob * (scale - outputs_mean) ** 2, axis=1))
         mse_std_loss = loss(outputs_std, 5.0 * y_test_std)
-
+        
         logit = np.log(prob + 1e-6)
-        ce_loss = -np.mean(np.ravel(logit * score_prob * ce_weight))
-        raw_ce_loss = -np.mean(np.ravel(logit * score_prob))
-        emd_loss = np.sqrt(np.mean(np.ravel((np.cumsum(prob,axis=1) - np.cumsum(score_prob,axis=1))**2)))
-        brier_score = np.mean(np.ravel((prob - score_prob)**2))
+        ce_loss = -np.mean(np.sum(logit * score_prob * ce_weight, axis=1))
+        raw_ce_loss = -np.mean(np.sum(logit * score_prob, axis=1))
+        emd_loss = np.mean(np.linalg.norm(np.cumsum(prob,axis=1) - np.cumsum(score_prob,axis=1), ord=2, axis=1))
+        brier_score = np.mean(np.mean((prob - score_prob)**2, axis=1))
 
         print(f"Test MSE Mean loss: {mse_mean_loss}, Test MSE Std loss: {mse_std_loss}")
         print(f"Test CE loss: {ce_loss}, Test Raw CE loss: {raw_ce_loss}")

train_flow.py
@@ -4,8 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import pandas as pd
-from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from torchvision.models import resnet50
@@ -25,7 +23,6 @@ def train_with_flow(model_resnet, model_flow, train_dataloader, optimizer_resnet
 
     progress_bar = tqdm(train_dataloader, leave=False)
     scale = torch.arange(1, 5.5, 0.5).to(device)
-    sqrt_2pi = np.sqrt(2 * np.pi)
     feature_extractor = create_feature_extractor(model_resnet, return_nodes={'layer4': 'layer4'})
 
     for images, mean_scores, std_scores, score_prob in progress_bar:
@@ -73,7 +70,6 @@ def evaluate_with_flow(model_resnet, model_flow, dataloader, device):
     running_brier_score = 0.0
 
     scale = torch.arange(1, 5.5, 0.5).to(device)
-    sqrt_2pi = np.sqrt(2 * np.pi)
     progress_bar = tqdm(dataloader, leave=False)
     feature_extractor = create_feature_extractor(model_resnet, return_nodes={'layer4': 'layer4'})
     with torch.no_grad():
@@ -92,9 +88,9 @@ def evaluate_with_flow(model_resnet, model_flow, dataloader, device):
             # Evaluate Normalizing Flow model for score_prob
             batch_scale = scale.repeat(len(images), 1).view(-1,1)
             batch_context = context.repeat(1,len(scale)).view(-1,context.shape[-1])
-            log_prob_score_prob = model_flow.log_prob(batch_scale, batch_context) # Use features as context for score_prob prediction
-            log_prob_score_prob = log_prob_score_prob.view(-1,len(scale))
-            prob = torch.exp(log_prob_score_prob)
+            log_prob = model_flow.log_prob(batch_scale, batch_context) # Use features as context for score_prob prediction
+            log_prob = log_prob.view(-1,len(scale))
+            prob = torch.exp(log_prob)
             kld_loss = model_flow.forward_kld(batch_scale, batch_context)
             
             # MSE loss for mean
@@ -111,10 +107,8 @@ def evaluate_with_flow(model_resnet, model_flow, dataloader, device):
             emd_loss = criterion_emd(prob, score_prob)
 
             # Cross-entropy loss
-            # ce_loss = criterion_weight_ce(log_prob_score_prob, score_prob)
-            # raw_ce_loss = criterion_raw_ce(log_prob_score_prob, score_prob)
-            ce_loss = -torch.mean(log_prob_score_prob * score_prob * ce_weight)
-            raw_ce_loss = -torch.mean(log_prob_score_prob * score_prob)
+            ce_loss = -torch.mean(torch.sum(log_prob * score_prob * ce_weight, dim=1))
+            raw_ce_loss = -torch.mean(torch.sum(log_prob * score_prob, dim=1))
 
             running_kld_loss += kld_loss.item()
             running_emd_loss += emd_loss.item()
@@ -148,6 +142,7 @@ use_attr = False
 use_hist = True
 is_resume = False
 
+
 if __name__ == '__main__':
     # Set random seed for reproducibility
     random_seed = 42
@@ -158,7 +153,7 @@ if __name__ == '__main__':
 
     lr = 5e-4
     batch_size = 32
-    num_epochs = 50
+    num_epochs = 30
     if is_log:
         wandb.init(project="resnet_PARA_GIAA")
         wandb.config = {
@@ -194,10 +189,10 @@ if __name__ == '__main__':
 
     # Define the number of classes in your dataset
     if use_attr:
-        num_classes = 9 + 5 * 7
-    else:
         num_classes = 9
-
+    else:
+        num_classes = 1
+    
     # Define the device for training
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -206,7 +201,7 @@ if __name__ == '__main__':
     # Modify the last fully connected layer to match the number of classes
     num_features = model_resnet50.fc.in_features
     model_resnet50.fc = nn.Linear(num_features, num_classes)
-    model_resnet50.load_state_dict(torch.load('best_model_resnet50_cls_lr1e-03_100epoch_noattr_ce.pth'))
+    model_resnet50.load_state_dict(torch.load('best_model_resnet50_noattr.pth'))
     # Move the model to the device
     model_resnet50 = model_resnet50.to(device)
 
@@ -228,7 +223,7 @@ if __name__ == '__main__':
     model_flow = nf.ConditionalNormalizingFlow(q0, flows)
     # FLAG
     if is_resume:
-        model_flow.load_state_dict(torch.load('best_model_resnet50_flow_lr5e-04_50epoch_noattr.pth'))
+        model_flow.load_state_dict(torch.load('best_model_flow_lr5e-04_50epoch_noattr.pth'))
     model_flow = model_flow.to(device)
     
     # Define the loss function
@@ -246,7 +241,7 @@ if __name__ == '__main__':
     # Initialize the best test loss and the best model
     best_test_loss = float('inf')
     best_model = None
-    best_modelname = 'best_model_resnet50_flow_lr%1.0e_%depoch' % (lr, num_epochs)
+    best_modelname = 'best_model_flow_lr%1.0e_%depoch' % (lr, num_epochs)
     if not use_attr:
         best_modelname += '_noattr'
     if is_resume:

train_resnet_cls.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
     # Modify the last fully connected layer to match the number of classes
     num_features = model_resnet50.fc.in_features
     model_resnet50.fc = nn.Linear(num_features, num_classes)
-
+    model_resnet50.load_state_dict(torch.load("best_model_resnet50_noattr.pth"))
     # model_resnet50.load_state_dict(torch.load("best_model_resnet50_cls_lr1e-03_30epoch_noattr.pth"))
     
     # Move the model to the device

train_resnet_dup.py
@@ -16,24 +16,36 @@ from train_resnet_cls import earth_mover_distance
 from scipy.stats import norm
 
 
-def earth_mover_distance_to_cdf(scale, mu, sigma, y):
+
+def metric_to_cdf(scale, mu, sigma, py, ce_weight=None, debug=False):
     """
     Compute Earth Mover's Distance (EMD) between two 1D tensors x and y using 2-norm.
     """
-    half_res = (scale[1] - scale[0]) * 0.5
+    half_res = ((scale[1] - scale[0]) * 0.5)
     x1 = (scale + half_res).cpu().numpy()
     x0 = (scale - half_res).cpu().numpy()
     mu = mu.cpu().numpy()
     sigma = sigma.cpu().numpy()
-    x = torch.tensor(np.array([norm.cdf(x1, loc=_mu, scale=_sigma) - norm.cdf(x0, loc=_mu, scale=_sigma) for _mu, _sigma in zip(mu, sigma)])).to(y.device)
-    cdf_x = torch.cumsum(x, dim=1)
-    x_norm = cdf_x[:,-1][...,None]
-    cdf_x = cdf_x / x_norm
-    x = x / x_norm
-    cdf_y = torch.cumsum(y, dim=1)
-    emd = torch.mean(torch.norm(cdf_x - cdf_y, p=2, dim=1))
-    brier_score = torch.mean((x - y)**2)
-    return emd, brier_score
+    px = torch.tensor(np.array([norm.cdf(x1, loc=_mu, scale=_sigma) - norm.cdf(x0, loc=_mu, scale=_sigma) for _mu, _sigma in zip(mu, sigma)])).to(py.device)
+    cdf_px = torch.cumsum(px, dim=1)
+    cdf_py = torch.cumsum(py, dim=1)
+    emd = torch.mean(torch.norm(cdf_px - cdf_py, p=2, dim=1))
+    brier_score = torch.mean((px - py)**2)
+
+    if debug:
+        scale = scale.cpu().numpy()
+        sqrt_2pi = np.sqrt(2*np.pi)
+        prob = np.exp(-0.5*((mu - scale) / sigma)**2) / sigma / sqrt_2pi * 0.5
+        print(prob / px.cpu().numpy())
+    
+    logprob = torch.log(px + 1e-6)
+    raw_ce = - torch.mean(torch.sum(logprob * py, dim=1))
+    if ce_weight is not None:
+        ce = - torch.mean(torch.sum(logprob * py * ce_weight, dim=1))
+    else:
+        raw_ce = ce
+
+    return emd, brier_score, ce, raw_ce
 
 
 def train(model, train_dataloader, optimizer, device):
@@ -65,11 +77,9 @@ def train(model, train_dataloader, optimizer, device):
         loss_std = criterion(output_std_score, std_scores)
         prob = torch.exp(-0.5*((output_mean_score - scale) / output_std_score)**2) / output_std_score / sqrt_2pi
         prob = prob / torch.sum(prob, dim=1, keepdim=True)
-        logit = torch.log(prob + 1e-6)
-        # raw_ce_loss = criterion_raw_ce(logit, score_prob)
-        # ce_loss = criterion_weight_ce(logit, score_prob)
-        ce_loss = -torch.mean(logit * score_prob * ce_weight)
-        raw_ce_loss = -torch.mean(logit * score_prob)
+        logprob = torch.log(prob + 1e-6)
+        ce_loss = -torch.mean(torch.sum(logprob * score_prob * ce_weight, dim=1))
+        raw_ce_loss = -torch.mean(torch.sum(logprob * score_prob, dim=1))
         emd_loss = criterion_emd(prob, score_prob)
 
         if loss_func == 'ce':
@@ -126,21 +136,8 @@ def evaluate(model, dataloader, device):
             num_classes = outputs.shape[1] // 2
             output_mean_score = outputs[:, :num_classes]
             output_std_score = outputs[:, num_classes:]
-            prob = torch.exp(-0.5*((output_mean_score - scale) / output_std_score)**2) / output_std_score / sqrt_2pi
             
-            # Discrete
-            # norm_prob = prob / torch.sum(prob, dim=1, keepdim=True)
-            # logit = torch.log(norm_prob + 1e-6)
-            # ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            # raw_ce_loss = -torch.mean(logit * score_prob)
-            # emd_loss = criterion_emd(norm_prob, score_prob)
-            
-            # Continuous
-            ce_offset = -(torch.log(output_std_score) + 0.5 * np.log(2*np.pi)) * 4.5
-            logit = - 0.5 * ((scale + 0.25 - output_mean_score)**3 - (scale - 0.25 - output_mean_score)**3) / 3 / output_std_score**2 + ce_offset
-            ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            raw_ce_loss = -torch.mean(logit * score_prob)
-            emd_loss, brier_score = earth_mover_distance_to_cdf(scale, output_mean_score, output_std_score, score_prob)
+            emd_loss, brier_score, ce_loss, raw_ce_loss = metric_to_cdf(scale, output_mean_score, output_std_score, score_prob, ce_weight=ce_weight)
 
             loss_mean = criterion(output_mean_score, mean_scores)
             loss_std = criterion(output_std_score, std_scores)
@@ -179,6 +176,7 @@ is_log = False
 use_attr = False
 use_hist = True
 loss_func = 'ce' # mse / emd / ce
+
 
 if __name__ == '__main__':
     # Set random seed for reproducibility
@@ -240,8 +238,11 @@ if __name__ == '__main__':
     # Modify the last fully connected layer to match the number of classes
     num_features = model_resnet50.fc.in_features
     model_resnet50.fc = nn.Linear(num_features, num_classes)
-    # model_resnet50.load_state_dict(torch.load('best_model_resnet50_dup_emd_lr1e-03_30epoch_noattr.pth'))
-    model_resnet50.load_state_dict(torch.load('best_model_resnet50_dup_mse_lr1e-03_30epoch_noattr.pth'))
+    if is_eval:
+        model_resnet50.load_state_dict(torch.load('best_model_resnet50_dup_mse_lr1e-03_30epoch_noattr.pth'))
+    else:
+        model_resnet50.load_state_dict(torch.load('best_model_resnet50_noattr.pth'), strict=False)
+    
     # Move the model to the device
     model_resnet50 = model_resnet50.to(device)
 

train_resnetll_cls_uc.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 from PARA_dataloader import PARADataset
 import wandb
 from train_resnet_cls import earth_mover_distance
-from train_resnet_dup import earth_mover_distance_to_cdf
+from train_resnet_dup import metric_to_cdf
 
 
 def train(model, train_dataloader, criterion, optimizer, device):
@@ -82,8 +82,8 @@ def evaluate(model, dataloader, criterion, ce_weight, device, num_samples=50):
             # prob = torch.exp(-0.5 * ((batch_outputs_mean - scale) / batch_outputs_std) ** 2) / batch_outputs_std / sqrt_2pi
             # prob = prob / torch.sum(prob, dim=1, keepdim=True)
             # logit = torch.log(prob + 1e-6)
-            ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            raw_ce_loss = -torch.mean(logit * score_prob)
+            ce_loss = criterion_weight_ce(logit, score_prob)
+            raw_ce_loss = criterion_raw_ce(logit, score_prob)
             emd_loss = criterion_emd(prob, score_prob)
             brier_score = criterion(prob, score_prob)
 
@@ -130,9 +130,9 @@ if __name__ == '__main__':
     random.seed(random_seed)
     # random_seed = None
 
-    lr = 5e-4
+    lr = 1e-3
     batch_size = 32
-    num_epochs = 10
+    num_epochs = 30
     if is_log:
         wandb.init(project="resnet_PARA_GIAA")
         wandb.config.update({

train_resnetll_uc.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 from PARA_dataloader import PARADataset
 import wandb
 from train_resnet_cls import earth_mover_distance
-from train_resnet_dup import earth_mover_distance_to_cdf
+from train_resnet_dup import metric_to_cdf
 
 
 def train(model, train_dataloader, criterion, optimizer, device):
@@ -83,20 +83,7 @@ def evaluate(model, dataloader, criterion, ce_weight, device, num_samples=50):
             batch_outputs_std = torch.std(batch_outputs, dim=0)
             batch_outputs = torch.stack([batch_outputs_mean, batch_outputs_std], dim=-1)
             dropout_outputs.append(batch_outputs)
-
-            # prob = torch.exp(-0.5 * ((batch_outputs_mean - scale) / batch_outputs_std) ** 2) / batch_outputs_std / sqrt_2pi
-            # prob = prob / torch.sum(prob, dim=1, keepdim=True)
-            # logit = torch.log(prob + 1e-6)
-            # ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            # raw_ce_loss = -torch.mean(logit * score_prob)
-            # emd_loss = criterion_emd(prob, score_prob)
-
-            # Continuous
-            ce_offset = -(torch.log(batch_outputs_std) + 0.5 * np.log(2*np.pi)) * 4.5
-            logit = - 0.5 * ((scale + 0.25 - batch_outputs_mean)**3 - (scale - 0.25 - batch_outputs_mean)**3) / 3 / batch_outputs_std**2 + ce_offset
-            ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            raw_ce_loss = -torch.mean(logit * score_prob)
-            emd_loss, brier_score = earth_mover_distance_to_cdf(scale, batch_outputs_mean, batch_outputs_std, score_prob)
+            emd_loss, brier_score, ce_loss, raw_ce_loss = metric_to_cdf(scale, batch_outputs_mean, batch_outputs_std, score_prob, ce_weight=ce_weight)
 
             mse_mean_loss = criterion(batch_outputs_mean, mean_scores)
             mse_std_loss = criterion(batch_outputs_std, std_scores)
@@ -195,9 +182,9 @@ if __name__ == '__main__':
     ce_weight = 1 / train_dataset.aesthetic_score_hist_prob
     ce_weight = ce_weight / np.sum(ce_weight) * len(ce_weight)
     ce_weight = torch.tensor(ce_weight, device=device)
-    criterion_weight_ce = nn.CrossEntropyLoss(weight=ce_weight)
-    criterion_raw_ce = nn.CrossEntropyLoss()
-    criterion_emd = earth_mover_distance
+    # criterion_weight_ce = nn.CrossEntropyLoss(weight=ce_weight)
+    # criterion_raw_ce = nn.CrossEntropyLoss()
+    # criterion_emd = earth_mover_distance
 
     optimizer_resnet50 = optim.SGD(model_resnet50.fc.parameters(), lr=lr, momentum=0.9)
 

train_resnetll_uc_ensemble.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 from PARA_dataloader import PARADataset
 import wandb
 from train_resnet_cls import earth_mover_distance
-from train_resnet_dup import earth_mover_distance_to_cdf
+from train_resnet_dup import metric_to_cdf
 from train_resnetll_uc import evaluate, custom_criterion
 
 
@@ -44,19 +44,7 @@ def evaluate_ensemble(dataloader, criterion, criterion_raw_ce, criterion_emd, de
             batch_outputs = torch.stack([batch_outputs_mean, batch_outputs_std], dim=-1)
             dropout_outputs.append(batch_outputs)
 
-            # prob = torch.exp(-0.5 * ((batch_outputs_mean - scale) / batch_outputs_std) ** 2) / batch_outputs_std / sqrt_2pi
-            # prob = prob / torch.sum(prob, dim=1, keepdim=True)
-            # logit = torch.log(prob + 1e-6)
-            # ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            # raw_ce_loss = -torch.mean(logit * score_prob)
-            # emd_loss = criterion_emd(prob, score_prob)
-
-            # Continuous
-            ce_offset = -(torch.log(batch_outputs_std) + 0.5 * np.log(2*np.pi)) * 4.5
-            logit = - 0.5 * ((scale + 0.25 - batch_outputs_mean)**3 - (scale - 0.25 - batch_outputs_mean)**3) / 3 / batch_outputs_std**2 + ce_offset
-            ce_loss = -torch.mean(logit * score_prob * ce_weight)
-            raw_ce_loss = -torch.mean(logit * score_prob)
-            emd_loss, brier_score = earth_mover_distance_to_cdf(scale, batch_outputs_mean, batch_outputs_std, score_prob)
+            emd_loss, brier_score, ce_loss, raw_ce_loss = metric_to_cdf(scale, batch_outputs_mean, batch_outputs_std, score_prob, ce_weight=ce_weight)
 
             mse_mean_loss = criterion(batch_outputs_mean, mean_scores)
             mse_std_loss = criterion(batch_outputs_std, std_scores)
@@ -110,7 +98,7 @@ if __name__ == '__main__':
         })
 
     root_dir = '/home/lwchen/datasets/PARA/'
-
+    mcdo_dir = 'mcdo_exp'
     train_transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.ToTensor(),
@@ -182,10 +170,10 @@ if __name__ == '__main__':
         if not use_uc:
             best_modelname += '_nouc'
         best_modelname += '.pth'
-        
+        best_modelname = os.path.join(mcdo_dir, best_modelname)
         model_resnet50.load_state_dict(torch.load(best_modelname), strict=False)
         model_resnet50 = model_resnet50.to(device)
-
+        
         test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
         test_mse_loss, test_mse_std_loss, test_custom_loss, test_ce_loss, test_raw_ce_loss, test_emd_loss, test_brier_score, dropout_mean, dropout_std = evaluate(