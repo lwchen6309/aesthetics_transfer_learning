0f40e113685e3c6cbd3fc7276b8280ad51d69d88
vsc36208 <vsc36208@r22g35.genius.hpc.kuleuven.be>
2024-05-13 21:14:47 +0200

    Setup PA-IAA GIAA-pretraining

--- train_paiaa_giaa.py
+++ train_paiaa_giaa.py
@@ -11,7 +11,7 @@ import numpy as np
 from tqdm import tqdm
 import wandb
 from scipy.stats import spearmanr
-from PARA_histogram_dataloader import load_data_paiaa, collate_fn_imgsort
+from PARA_histogram_dataloader import load_data, collate_fn_imgsort
 from train_nima import earth_mover_distance
 import math
 
@@ -150,15 +150,15 @@ def train(model, dataloader, piaa_dataloader, optimizer, device):
         optimizer.zero_grad()
         aesthetic_logits, big5_pred = model(images)
         prob_aesthetic = F.softmax(aesthetic_logits, dim=1)
-        # loss_aesthetic = earth_mover_distance(prob_aesthetic, aesthetic_score_histogram).mean()
-        loss_aesthetic = criterion_mse(prob_aesthetic, aesthetic_score_histogram)
+        loss_aesthetic = earth_mover_distance(prob_aesthetic, aesthetic_score_histogram).mean()
+        # loss_aesthetic = criterion_mse(prob_aesthetic, aesthetic_score_histogram)
         
         loss_aesthetic.backward()
         optimizer.step()
         running_aesthetic_dist_mse_loss += loss_aesthetic.item()
 
         progress_bar.set_postfix({
-            'Train MSE Loss for ScoreDistribution': loss_aesthetic.item(),
+            'Train EMD Loss for aesthetic': loss_aesthetic.item(),
         })
 
     # Train big5
@@ -207,6 +207,7 @@ def evaluate(model, dataloader, piaa_dataloader, device):
             aesthetic_logits, big5_pred = model(images)
             prob_aesthetic = F.softmax(aesthetic_logits, dim=1)
             loss_aesthetic = earth_mover_distance(prob_aesthetic, aesthetic_score_histogram).mean()
+            # loss_aesthetic = criterion_mse(prob_aesthetic, aesthetic_score_histogram)
 
             outputs_mean = torch.sum(prob_aesthetic * scale, dim=1)
             target_mean = torch.sum(aesthetic_score_histogram * scale, dim=1)
@@ -235,8 +236,7 @@ def evaluate(model, dataloader, piaa_dataloader, device):
             progress_bar.set_postfix({
                 'Test MSE Loss for Big5': loss_aesthetic.item(),
             })
-            break
-
+    
     # Calculate SROCC
     predicted_scores = np.concatenate(mean_pred)
     true_scores = np.concatenate(mean_target)
@@ -248,117 +248,33 @@ def evaluate(model, dataloader, piaa_dataloader, device):
     return epoch_mse_loss_aesthetic, epoch_mse_loss_big5, srocc, mse_loss
 
 
-num_bins = 9
-# num_attr = 8
-# num_bins_attr = 5
-# num_pt = 50 + 20
-criterion_mse = nn.MSELoss()
+def trainer(dataloaders, model, optimizer, args, train_fn, evaluate_fn, device, best_modelname):
+    
+    train_dataloader, train_piaa_dataloader, val_giaa_dataloader, val_piaa_imgsort_dataloader, test_giaa_dataloader, test_piaa_imgsort_dataloader = dataloaders
 
-
-if __name__ == '__main__':    
-    parser = argparse.ArgumentParser(description='Training and Testing the Combined Model for data spliting')
-    parser.add_argument('--trainset', type=str, default='GIAA', choices=["GIAA", "sGIAA", "PIAA"])
-    parser.add_argument('--fold_id', type=int, default=1)
-    parser.add_argument('--n_fold', type=int, default=4)
-    parser.add_argument('--resume', type=str, default=None)
-    parser.add_argument('--use_cv', action='store_true', help='Enable cross validation')
-    parser.add_argument('--is_eval', action='store_true', help='Enable evaluation mode')
-    parser.add_argument('--no_log', action='store_false', dest='is_log', help='Disable logging')
-    args = parser.parse_args()
-    
-    resume = args.resume
-    is_eval = args.is_eval
-    is_log = args.is_log
-    
-    random_seed = 42
-    lr = 5e-5
-    batch_size = 100
-    num_epochs = 1
-    lr_schedule_epochs = 5
-    lr_decay_factor = 0.5
-    max_patience_epochs = 10
-    n_workers = 8
-    eval_on_giaa = True
-
-    if is_log:
-        tags = ["no_attr","GIAA"]
-        if args.use_cv:
-            tags += ["CV%d/%d"%(args.fold_id, args.n_fold)]
-        wandb.init(project="resnet_PARA_PIAA", 
-                   notes="PAIAA-GIAA",
-                   tags = tags)
-        wandb.config = {
-            "learning_rate": lr,
-            "batch_size": batch_size,
-            "num_epochs": num_epochs
-        }
-        experiment_name = wandb.run.name
-    else:
-        experiment_name = ''
-    
-    train_dataset, train_piaa_imgsort_dataset, val_giaa_dataset, val_piaa_imgsort_dataset, test_giaa_dataset, test_piaa_dataset, test_piaa_imgsort_dataset = load_data_paiaa(args)
-    # Create dataloaders
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_workers, timeout=300)
-    train_piaa_dataloader = DataLoader(train_piaa_imgsort_dataset, batch_size=5, shuffle=True, num_workers=n_workers, timeout=300, collate_fn=collate_fn_imgsort)
-
-    val_giaa_dataloader = DataLoader(val_giaa_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, timeout=300)
-    val_piaa_imgsort_dataloader = DataLoader(val_piaa_imgsort_dataset, batch_size=5, shuffle=False, num_workers=n_workers, timeout=300, collate_fn=collate_fn_imgsort)
-    test_giaa_dataloader = DataLoader(test_giaa_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, timeout=300)
-    test_piaa_imgsort_dataloader = DataLoader(test_piaa_imgsort_dataset, batch_size=5, shuffle=False, num_workers=n_workers, timeout=300, collate_fn=collate_fn_imgsort)
-    # test_user_piaa_dataloader = DataLoader(test_user_piaa_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, timeout=300)
-    
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # Initialize the combined model
-    model_ft = resnet50(pretrained=True)
-    model_ft.aux_logits = False
-    num_ftrs = model_ft.fc.out_features
-    net1 = AesModel(num_bins, 0.5, num_ftrs)
-    net2 = PerModel(0.5, num_ftrs)
-    model = convNet(resnet=model_ft, aesnet=net1, pernet=net2).to(device)
-    
-    if resume is not None:
-        model.load_state_dict(torch.load(resume))
-    # Loss and optimizer
-    # optimizer = optim.Adam(model.parameters(), lr=lr)
-    optimizer = optim.Adam([
-        {'params': model.AesNet.parameters(), 'lr': 1e-3},
-        {'params': model.PerNet.parameters(), 'lr': 1e-3},
-        {'params': model.resnet.parameters(), 'lr': lr}
-    ], lr=lr)  # Default LR, applies to parameters not explicitly set above
-    
-    # Initialize the best test loss and the best model
-    best_model = None
-    best_modelname = 'best_model_resnet50_paiaa_giaa_lr%1.0e_decay_%depoch' % (lr, num_epochs)
-    best_modelname += '_%s'%experiment_name
-    best_modelname += '.pth'
-    dirname = 'models_pth'
-    if args.use_cv:
-        dirname = os.path.join(dirname, 'random_cvs')
-    best_modelname = os.path.join(dirname, best_modelname)
-    
     # Training loop
     best_test_srocc = 0
-    for epoch in range(num_epochs):
-        if is_eval:
+    num_patience_epochs = 0
+    for epoch in range(args.num_epochs):
+        if args.is_eval:
             break
         # Learning rate schedule
-        if (epoch + 1) % lr_schedule_epochs == 0:
+        if (epoch + 1) % args.lr_schedule_epochs == 0:
             for param_group in optimizer.param_groups:
-                param_group['lr'] *= lr_decay_factor
+                param_group['lr'] *= args.lr_decay_factor
 
         # Training
-        train_mse_loss_aesthetic, train_mse_loss_big5 = train(model, train_dataloader, train_piaa_dataloader, optimizer, device)
-        if is_log:
-            wandb.log({"Train MSE Loss for aesthetic distribution": train_mse_loss_aesthetic,
+        train_loss_aesthetic, train_mse_loss_big5 = train_fn(model, train_dataloader, train_piaa_dataloader, optimizer, device)
+        if args.is_log:
+            wandb.log({"Train EMD Loss": train_loss_aesthetic,
                        "Train MSE Loss for Big5": train_mse_loss_big5,
                        }, commit=False)
         
         # Testing
-        val_mse_loss_aesthetic, val_mse_loss_big5, val_giaa_srocc, val_giaa_mse = evaluate(model, val_giaa_dataloader, val_piaa_imgsort_dataloader, device)
-        if is_log:
+        val_loss_aesthetic, val_mse_loss_big5, val_giaa_srocc, val_giaa_mse = evaluate_fn(model, val_giaa_dataloader, val_piaa_imgsort_dataloader, device)
+        if args.is_log:
             wandb.log({
-                "Val MSE Loss for aesthetic distribution": val_mse_loss_aesthetic,
+                "Val EMD Loss": val_loss_aesthetic,
                 "Val GIAA MSE Loss for Big5": val_mse_loss_big5,
                 "Val GIAA SROCC": val_giaa_srocc,
                 # "Val PIAA EMD Loss": val_piaa_emd_loss,
@@ -373,24 +289,116 @@ if __name__ == '__main__':
             torch.save(model.state_dict(), best_modelname)
         else:
             num_patience_epochs += 1
-            if num_patience_epochs >= max_patience_epochs:
-                print("Validation loss has not decreased for {} epochs. Stopping training.".format(max_patience_epochs))
+            if num_patience_epochs >= args.max_patience_epochs:
+                print("Validation loss has not decreased for {} epochs. Stopping training.".format(args.max_patience_epochs))
                 break
     
-    if not is_eval:
+    if not args.is_eval:
         model.load_state_dict(torch.load(best_modelname))   
     
     # Testing
-    test_mse_loss_aesthetic, test_mse_loss_big5, test_giaa_srocc, test_giaa_mse = evaluate(model, test_giaa_dataloader, test_piaa_imgsort_dataloader, device)
-    if is_log:
+    test_loss_aesthetic, test_mse_loss_big5, test_giaa_srocc, test_giaa_mse = evaluate_fn(model, test_giaa_dataloader, test_piaa_imgsort_dataloader, device)
+    if args.is_log:
         wandb.log({
-            "Test MSE Loss for aesthetic distribution": test_mse_loss_aesthetic,
+            "Test EMD Loss": test_loss_aesthetic,
             "Test GIAA MSE Loss for Big5": test_mse_loss_big5,
             "Test GIAA SROCC": test_giaa_srocc,
         }, commit=True)
     
     # Print the epoch loss
-    print(f"Epoch [{epoch + 1}/{num_epochs}], "
-            f"Test GIAA SROCC Loss: {test_giaa_srocc:.4f}, "
+    print(f"Test GIAA SROCC Loss: {test_giaa_srocc:.4f}, "
             f"Test GIAA MSE Loss: {test_giaa_mse:.4f}, "
-            )
+            )    
+
+num_bins = 9
+# num_attr = 8
+# num_bins_attr = 5
+# num_pt = 50 + 20
+criterion_mse = nn.MSELoss()
+
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description='Training and Testing the Combined Model for data spliting')
+    parser.add_argument('--trainset', type=str, default='GIAA', choices=["GIAA", "sGIAA", "PIAA"])
+    parser.add_argument('--fold_id', type=int, default=1)
+    parser.add_argument('--n_fold', type=int, default=4)
+    parser.add_argument('--resume', type=str, default=None)
+    parser.add_argument('--train_piaa_augment', type=bool, default=True)
+    parser.add_argument('--use_cv', action='store_true', help='Enable cross validation')
+    parser.add_argument('--is_eval', action='store_true', help='Enable evaluation mode')
+    parser.add_argument('--no_log', action='store_false', dest='is_log', help='Disable logging')
+    parser.add_argument('--num_epochs', type=int, default=20)
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--max_patience_epochs', type=int, default=10)
+    parser.add_argument('--lr', type=float, default=5e-5)
+    parser.add_argument('--lr_schedule_epochs', type=int, default=5)
+    parser.add_argument('--lr_decay_factor', type=float, default=0.5)
+
+    args = parser.parse_args()
+    resume = args.resume
+    batch_size = args.batch_size
+    random_seed = 42
+    n_workers = 8
+    eval_on_giaa = True
+
+    if args.is_log:
+        tags = ["no_attr","GIAA"]
+        if args.use_cv:
+            tags += ["CV%d/%d"%(args.fold_id, args.n_fold)]
+        wandb.init(project="resnet_PARA_PIAA", 
+                   notes="PAIAA-GIAA",
+                   tags = tags)
+        wandb.config = {
+            "learning_rate": args.lr,
+            "batch_size": batch_size,
+            "num_epochs": args.num_epochs
+        }
+        experiment_name = wandb.run.name
+    else:
+        experiment_name = ''
+    
+    train_dataset, train_piaa_imgsort_dataset, val_giaa_dataset, val_piaa_imgsort_dataset, test_giaa_dataset, test_piaa_imgsort_dataset = load_data(args)
+    
+    # Create dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_workers, timeout=300)
+    train_piaa_dataloader = DataLoader(train_piaa_imgsort_dataset, batch_size=5, shuffle=True, num_workers=n_workers, timeout=300, collate_fn=collate_fn_imgsort)
+
+    val_giaa_dataloader = DataLoader(val_giaa_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, timeout=300)
+    val_piaa_imgsort_dataloader = DataLoader(val_piaa_imgsort_dataset, batch_size=5, shuffle=False, num_workers=n_workers, timeout=300, collate_fn=collate_fn_imgsort)
+    test_giaa_dataloader = DataLoader(test_giaa_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, timeout=300)
+    test_piaa_imgsort_dataloader = DataLoader(test_piaa_imgsort_dataset, batch_size=5, shuffle=False, num_workers=n_workers, timeout=300, collate_fn=collate_fn_imgsort)
+    # test_user_piaa_dataloader = DataLoader(test_user_piaa_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, timeout=300)
+    
+    dataloaders = (train_dataloader, train_piaa_dataloader, val_giaa_dataloader, val_piaa_imgsort_dataloader, test_giaa_dataloader, test_piaa_imgsort_dataloader)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize the combined model
+    model_ft = resnet50(pretrained=True)
+    model_ft.aux_logits = False
+    num_ftrs = model_ft.fc.out_features
+    net1 = AesModel(num_bins, 0.5, num_ftrs)
+    net2 = PerModel(0.5, num_ftrs)
+    model = convNet(resnet=model_ft, aesnet=net1, pernet=net2).to(device)
+    
+    if args.resume is not None:
+        model.load_state_dict(torch.load(args.resume))
+    # Loss and optimizer
+    # optimizer = optim.Adam(model.parameters(), lr=lr)
+    optimizer = optim.Adam([
+        {'params': model.AesNet.parameters(), 'lr': 1e-3},
+        {'params': model.PerNet.parameters(), 'lr': 1e-3},
+        {'params': model.resnet.parameters(), 'lr': args.lr}
+    ], lr=args.lr)  # Default LR, applies to parameters not explicitly set above
+    
+    # Initialize the best test loss and the best model
+    best_model = None
+    best_modelname = 'best_model_resnet50_paiaa_giaa_lr%1.0e_decay_%depoch' % (args.lr, args.num_epochs)
+    best_modelname += '_%s'%experiment_name
+    best_modelname += '.pth'
+    dirname = 'models_pth'
+    if args.use_cv:
+        dirname = os.path.join(dirname, 'random_cvs')
+    best_modelname = os.path.join(dirname, best_modelname)
+    
+    trainer(dataloaders, model, optimizer, args, train, evaluate, device, best_modelname)
