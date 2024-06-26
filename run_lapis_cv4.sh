ce6969050a46318488c3ad144be5f60784789766
vsc36208 <vsc36208@tier2-p-login-2.genius.hpc.kuleuven.be>
2024-06-26 13:56:38 +0200

    add argflags

--- run_lapis_cv4.sh
+++ run_lapis_cv4.sh
@@ -11,12 +11,12 @@ run_script="train_histonet_latefusion_lapis_dou.py"
 # python $run_script --trainset GIAA --resume models_pth/lapis_best_model_resnet50_histo_lr5e-05_decay_20epoch_lucky-voice-471.pth --no_log --is_eval # PIAA
 
 # python $run_script --trainset GIAA
-python $run_script --trainset sGIAA-pair --batch_size 50
+# python $run_script --trainset sGIAA-pair --batch_size 50
 
-python $run_script --n_fold 4 --fold_id 1 --use_cv --trainset sGIAA-pair --batch_size 50
-python $run_script --n_fold 4 --fold_id 2 --use_cv --trainset sGIAA-pair --batch_size 50
-python $run_script --n_fold 4 --fold_id 3 --use_cv --trainset sGIAA-pair --batch_size 50
-python $run_script --n_fold 4 --fold_id 4 --use_cv --trainset sGIAA-pair --batch_size 50
+python $run_script --n_fold 4 --fold_id 1 --use_cv --trainset sGIAA-pair --batch_size 50 --dropout 0.5
+# python $run_script --n_fold 4 --fold_id 2 --use_cv --trainset sGIAA-pair --batch_size 50 --dropout 0.5
+# python $run_script --n_fold 4 --fold_id 3 --use_cv --trainset sGIAA-pair --batch_size 50 --dropout 0.5
+# python $run_script --n_fold 4 --fold_id 4 --use_cv --trainset sGIAA-pair --batch_size 50 --dropout 0.5
 
 # python $run_script --n_fold 4 --fold_id 1 --use_cv --trainset GIAA
 # python $run_script --n_fold 4 --fold_id 2 --use_cv --trainset GIAA
