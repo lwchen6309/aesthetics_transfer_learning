5eb3bddbbb40a6192a2f4aa082bbb7db89a36efe
lwchen <li-wei.chen@kuleuven.be>
2026-03-04 22:41:39 +0100

    chore: keep only HF additions relative to d724171

--- run_PARA_PIAA_torch2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-source /home/lwchen/anaconda3/bin/activate torch2
-cd /home/lwchen/active_nngp
-exec bash run_PARA_PIAA.sh "$@"
--- unified_iaa/__init__.py
+++
@@ -1,3 +0,0 @@
-from .api import UnifiedIAA
-
-__all__ = ["UnifiedIAA"]
